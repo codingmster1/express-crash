ff8acbf35e3e58edaf77c2723290a9968c283613
Jeffrey Doyle <doylejeff98@gmail.com>
2024-05-24 20:34:37 -0400

    posts filter tested

--- server.js
+++ server.js
@@ -26,4 +26,9 @@ app.get('/api/posts', (req, res) => {
 res.json(posts);
 })
 
+app.get('/api/posts/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    res.json(posts.filter((post) => post.id === id));
+    });
+
 app.listen(port, () => console.log(`Server is running on port ${port}`));
