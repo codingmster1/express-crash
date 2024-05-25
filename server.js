17ef15ac3d4e140e9b009512e73ad64f224fcd97
Jeffrey Doyle <doylejeff98@gmail.com>
2024-05-24 20:08:40 -0400

    api posts route

--- server.js
+++ server.js
@@ -3,12 +3,26 @@ const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => {
+// setup static folder 
+/* app.use(express.static(path.join(__dirname, 'public'))); */
+
+
+/* app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.get('/about', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'about.html'));
-});
+}); */
+
+let posts = [
+    {id: 1, title: 'Post one'},
+    {id: 2, title: 'Post two'},
+    {id: 3, title: 'Post three'}
+];
+
+app.get('/api/posts', (req, res) => {
+res.json(posts);
+})
 
 app.listen(8000, () => console.log(`Server is running on port 8000`));
